7665a37f302c1b5ee5fe459f11e7856dea91f785
roundRekt <roundrekt@icloud.com>
2026-01-10 23:59:02 +0800

    新增汉化偏好设置

--- src/index.js
+++ src/index.js
@@ -1,40 +1,50 @@
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    const filepath = url.pathname.slice(1);
-    const pathparts = filepath.split("/");
-    const filename = pathparts.at(-1);
-    const version = pathparts.shift();
-    const resourceType = pathparts[0];
-    const isCnVoice = url.hostname === "cn-voice.prod-clientpatch.bluearchive.cafe";
-    const ASSET_BUNDLES = new Set(["iOS_PatchPack", "Android_PatchPack"]);
-    const MEDIA_CATALOG = new Set(["MediaCatalog.hash", "MediaCatalog.bytes"]);
-    const ASSET_CATALOG = new Set([
-      "BundlePackingInfo.hash",
-      "BundlePackingInfo.bytes",
-      "catalog_Android.hash",
-      "catalog_Android.zip",
-      "catalog_iOS.hash",
-      "catalog_iOS.zip"
-    ]);
+    async fetch(request, env, ctx) {
+        const url = new URL(request.url);
+        const filepath = url.pathname.slice(1);
+        const pathparts = filepath.split("/");
+        const filename = pathparts.at(-1);
+        const version = pathparts.shift();
+        const type = pathparts[0];
+        const cookie = request.headers.get("Cookie") || ""
+        const cookies = Object.fromEntries(
+            cookie.split('; ').map(c => {
+                const [key, value] = c.split('=');
+                return [key, value];
+            })
+        );
+        const ASSET_CATALOG = new Set([
+            "BundlePackingInfo.hash",
+            "BundlePackingInfo.bytes",
+            "catalog_Android.hash",
+            "catalog_Android.zip",
+            "catalog_iOS.hash",
+            "catalog_iOS.zip"
+        ]);
+        const MEDIA_CATALOG = new Set([
+            "MediaCatalog.hash",
+            "MediaCatalog.bytes"
+        ]);
 
-    if (resourceType === "TableBundles") {
-      const object = await (isCnVoice ? env.TABLEBUNDLES_CN_VOICE : env.TABLEBUNDLES).get(filepath);
-      if (object) return new Response(object.body, { headers: { "Cache-Control": "public, max-age=3600" } });
-    }
+        if (cookies.dev === "true") {
+            const object = await env.CLIENTPATCH.get(filepath);
+            if (object) return new Response(object.body);
+            return Response.redirect("https://prod-clientpatch.bluearchiveyostar.com/" + filepath, 302);
+        }
 
-    if (ASSET_BUNDLES.has(resourceType)) {
-      const key = [ASSET_CATALOG.has(filename) ? version : "shared", ...pathparts].join("/");
-      const object = await env.ASSETBUNDLES.get(key);
-      if (object) return new Response(object.body, { headers: { "Cache-Control": "public, max-age=3600" } });
-    }
+        if (type === "TableBundles" && cookies.table === "cn") {
+            const object = await (cookies.voice === "cn" ? env.TABLEBUNDLES_CN_VOICE : env.TABLEBUNDLES).get(filepath);
+            if (object) return new Response(object.body);
+        } else if ((type === "Android_PatchPack" || type === "iOS_PatchPack") && cookies.asset === "cn") {
+            const key = [ASSET_CATALOG.has(filename) ? version : "shared", ...pathparts].join("/");
+            const object = await env.ASSETBUNDLES.get(key);
+            if (object) return new Response(object.body);
+        } else if (type === "MediaResources" && cookies.voice === "cn") {
+            const key = [MEDIA_CATALOG.has(filename) ? version : "shared", ...pathparts].join("/");
+            const object = await env.MEDIARESOURCES.get(key);
+            if (object) return new Response(object.body);
+        }
 
-    if (resourceType === "MediaResources" && isCnVoice) {
-      const key = [MEDIA_CATALOG.has(filename) ? version : "shared", ...pathparts].join("/");
-      const object = await env.MEDIARESOURCES.get(key);
-      if (object) return new Response(object.body, { headers: { "Cache-Control": "public, max-age=3600" } });
-    }
-
-    return Response.redirect("https://prod-clientpatch.bluearchiveyostar.com/" + filepath, 302);
-  },
+        return Response.redirect("https://prod-clientpatch.bluearchiveyostar.com/" + filepath, 302);
+    },
 };

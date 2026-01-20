760ccf3f694d51f27a44768cb369df46b9952002
roundRekt <roundrekt@icloud.com>
2026-01-20 18:11:42 +0800

    重定向适配中配

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export default {
         const key = new URL(request.url).pathname.slice(1);
         const object = await env.CLIENTPATCH.get(key);
         if (object) return new Response(object.body);
-        return Response.redirect("https://prod-clientpatch.bluearchiveyostar.com/" + key, 302);
+        key = key.replace("VOC_CN/", "");
+        return Response.redirect(`https://prod-clientpatch.bluearchiveyostar.com/${key}`, 302);
     },
 };

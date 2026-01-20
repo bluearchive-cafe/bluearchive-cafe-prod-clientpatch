f0e559f5b2f07970d01358ddeaa72139e57d3c0b
roundRekt <roundrekt@icloud.com>
2026-01-20 18:13:07 +0800

    重定向适配中配

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default {
     async fetch(request, env, ctx) {
-        const key = new URL(request.url).pathname.slice(1);
+        let key = new URL(request.url).pathname.slice(1);
         const object = await env.CLIENTPATCH.get(key);
         if (object) return new Response(object.body);
         key = key.replace("VOC_CN/", "");
